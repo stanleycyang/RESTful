b8fa340d2efad55c63c1447e11a2a698688b3090
Stanley Yang <stanleyang13@gmail.com>
2015-05-28 21:37:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // Connect MongoDB
-mongoose.connect('mongodb://stan:foobar@ds035702.mongolab.com:35702/heroku_app37202379');
+mongoose.connect('INSERT LINK');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
